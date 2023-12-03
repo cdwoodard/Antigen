82e7860e45c83ad49ab04057a974e1bcefc5e4cc
allycao <caos@uchicago.edu>
2023-12-03 01:38:57 -0600

    instantiate multiple enemies

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -8,7 +8,9 @@ public class Main : MonoBehaviour {
 
     [Header("Inscribed")]
     public TextMeshProUGUI uitChemokines;
-
+    public bool spawnPathogens = true;
+    public GameObject[] prefabPathogens;
+    public float pathogenSpawnPerSecond = 0.5f;
 
     //map of all types and costs
     public GameObject[] cells;
@@ -22,6 +24,11 @@ public class Main : MonoBehaviour {
         private set;
     }
 
+    //awake and spawn enemies
+    public void Awake(){
+        Invoke(nameof(SpawnPathogen), 1f / pathogenSpawnPerSecond);
+    }
+
     public static List<GameObject> epidermalCells = new List<GameObject>();
 
     // Start is called before the first frame update
@@ -32,6 +39,31 @@ public class Main : MonoBehaviour {
         for(int i = 0; i < cells.Length; i++){
             priceMap.Add(cells[i], prices[i]);
         }
+    }
+
+    public void SpawnPathogen()
+    {
+        if (!spawnPathogens)
+        {
+            Invoke(nameof(SpawnPathogen), 1f / pathogenSpawnPerSecond);
+            return;
+        }
+
+        // Pick a random Pathogen to instantiate
+        int ndx = Random.Range(0, prefabPathogens.Length);
+        GameObject go = Instantiate<GameObject>(prefabPathogens[ndx]);
+
+        // Put the pathogen in a random y position at the right of the screen
+        float pathogenYpos = Random.Range(-5f,5f); //randomly pick from 0 to 1
+        
+        // Set the initial position for the spawned pathogen
+        Vector3 pos = Vector3.zero;
+        pos.x = 10f;
+        pos.y = pathogenYpos;
+        go.transform.position = pos;
+
+        // Invoke SpawnPathogen Again
+        Invoke(nameof(SpawnPathogen), 1f / pathogenSpawnPerSecond);
     }
 
     void UpdateGUI() {
