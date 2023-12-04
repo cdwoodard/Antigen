bcc5c350f399439e568d14fc8241e27a786d339a
Kanchan Naik <kanchan@uchicago.edu>
2023-12-04 03:37:19 -0600

    encyclopedia feature + new sprites + buttons/panels

--- Assets/Scripts/Encyclopedia.cs
+++ Assets/Scripts/Encyclopedia.cs
@@ -7,6 +7,31 @@ public class Encyclopedia : MonoBehaviour {
     public SpriteRenderer spriteRenderer;
     public Sprite updatedEncyclopedia; // sprite appearance once a new enemy is found
 
+    // General Sprites
+    public GameObject EncyclopediaPanel;
+    public GameObject EncyclopediaImage;
+    public GameObject BackButton;
+
+    // Chicken Pox Sprites
+    public GameObject ChickenPox_Image;
+    public GameObject ChickenPox_Text;
+    public GameObject ChickenPox_Resistance;
+
+    //Strep Sprites
+    public GameObject Strep_Image;
+    public GameObject Strep_Text;
+    public GameObject Strep_Resistance;
+
+    // Adenovirus Sprites
+    public GameObject Aden_Image;
+    public GameObject Aden_Text;
+    public GameObject Aden_Resistance;
+
+    // Myco Sprites
+    public GameObject Myco_Image;
+    public GameObject Myco_Text;
+    public GameObject Myco_Resistance;
+
     public struct entry{
         public Sprite image;
         public int strength;
@@ -23,6 +48,7 @@ public class Encyclopedia : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start(){
+        print("hello?");
         E = this;
         spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
     }
@@ -35,6 +61,72 @@ public class Encyclopedia : MonoBehaviour {
     public static void addEntry(string type, Sprite image){
         encyclopedia.Add(type, new entry(image, 1));
         E.SwitchSprite();
+    }
+
+    public void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+        {
+            print("on Mouse Down?");
+            Time.timeScale = 0;
+            EncyclopediaPanel.SetActive(true);
+            EncyclopediaImage.SetActive(true);
+            BackButton.SetActive(true);
+
+            if (encyclopedia.ContainsKey("Chickenpox"))
+            {
+                ChickenPox_Image.SetActive(true);
+                ChickenPox_Text.SetActive(true);
+                ChickenPox_Resistance.SetActive(true);
+            }
+
+            if (encyclopedia.ContainsKey("Streptococcus"))
+            {
+                Strep_Image.SetActive(true);
+                Strep_Text.SetActive(true);
+                Strep_Resistance.SetActive(true);
+            }
+
+            if (encyclopedia.ContainsKey("Adenovirus"))
+            {
+                Aden_Image.SetActive(true);
+                Aden_Text.SetActive(true);
+                Aden_Resistance.SetActive(true);
+            }
+
+            if (encyclopedia.ContainsKey("Mycobacterium ulcerans"))
+            {
+                Myco_Image.SetActive(true);
+                Myco_Text.SetActive(true);
+                Myco_Resistance.SetActive(true);
+            }
+
+        }
+    }
+
+    public void BackToGame()
+    {
+        EncyclopediaPanel.SetActive(false);
+        EncyclopediaImage.SetActive(false);
+        BackButton.SetActive(false);
+
+        ChickenPox_Image.SetActive(false);
+        ChickenPox_Text.SetActive(false);
+        ChickenPox_Resistance.SetActive(false);
+
+        Strep_Image.SetActive(false);
+        Strep_Text.SetActive(false);
+        Strep_Resistance.SetActive(false);
+
+        Aden_Image.SetActive(false);
+        Aden_Text.SetActive(false);
+        Aden_Resistance.SetActive(false);
+
+        Myco_Image.SetActive(false);
+        Myco_Text.SetActive(false);
+        Myco_Resistance.SetActive(false);
+
+        Time.timeScale = 1;
     }
 
 }
